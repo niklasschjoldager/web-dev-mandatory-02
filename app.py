a55faf1c67800c70013ca6c7e2862c6db71b977e
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-10 15:48:19 +0100

    Init

---
+++ app.py
@@ -0,0 +1,18 @@
+from bottle import default_app, get, run, view
+
+############################################################
+@get("/")
+@view("index")
+def _():
+    return
+
+
+############################################################
+try:
+    # Production
+    import production
+
+    application = default_app()
+except:
+    # Development
+    run(host="127.0.0.1", port=3333, debug=True, reloader=True)
