f79aca91afa28ab7d2a97c95e6dcb7c2b36ed0b9
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-11 16:54:35 +0100

    Todo comments edited

--- api/tweets/tweets_by_id_get.py
+++ api/tweets/tweets_by_id_get.py
@@ -4,7 +4,6 @@ from bottle import get
 @get("/tweets/<tweet_id>")
 def _(tweet_id):
     # Validate
-    # Who likes it? The user who is logged in
     # Connect to the db
-    # Update / insert the liked tweet
+    # Send the tweet data back
     return "Tweet received"
--- api/tweets/tweets_get.py
+++ api/tweets/tweets_get.py
@@ -4,7 +4,6 @@ from bottle import get
 @get("/tweets")
 def _():
     # Validate
-    # Who likes it? The user who is logged in
     # Connect to the db
-    # Update / insert the liked tweet
+    # Send the tweets data back
     return "Tweets received"
