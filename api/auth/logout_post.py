50403a9e91ab5238a4d75d81451648d8113b2001
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-17 20:20:05 +0100

    Bug fix: Logout remove user session now works correctly

--- api/auth/logout_post.py
+++ api/auth/logout_post.py
@@ -1,18 +1,27 @@
 from bottle import get, redirect, response, request, post
 
-from g import user_sessions
+import jwt
+
+from g import JSON_WEB_TOKEN_SECRET, user_sessions
 
 ############################################################
 @post("/logout")
 @get("/logout")
 def _():
-    if not request.get_cookie("user_session"):
-        redirect("/")
+    try:
+        if not request.get_cookie("user_session"):
+            return redirect("/")
 
-    user_session = request.get_cookie("user_session")
-    if user_session in user_sessions:
-        user_sessions.remove(user_session)
+        encoded_user_session = request.get_cookie("user_session")
+        jwt_decoded = jwt.decode(encoded_user_session, JSON_WEB_TOKEN_SECRET, algorithms=["HS256"])
 
-    response.delete_cookie("user_session")
+        for index, session in enumerate(user_sessions):
+            if jwt_decoded["session_id"] == session:
+                user_sessions.pop(index)
 
-    return redirect("/")
+        response.delete_cookie("user_session")
+        return redirect("/")
+    except jwt.exceptions.InvalidTokenError as ex:
+        print(ex)
+        response.delete_cookie("user_session")
+        return redirect("/")
