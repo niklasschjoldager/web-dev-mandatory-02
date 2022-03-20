27023538dd9dfe6d14662d9e90baa95f6713bb55
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-20 21:36:38 +0100

    Feature: Tweet form textarea now resizes automatically

--- js/home.js
+++ js/home.js
@@ -4,8 +4,42 @@ const inputAddMedia = formCreateTweet.querySelector("#tweet-media")
 const templateTweet = document.querySelector("[data-template=tweet]")
 const hookTweets = document.querySelector("[data-hook=tweets]")
 
-formCreateTweet.addEventListener("submit", handleCreateTweet)
+const tweetText = document.querySelector("[data-hook=tweet-text]")
+initTweetTextResize()
+
+function initTweetTextResize() {
+  tweetText.style.height = `${tweetText.scrollHeight}px`
+  tweetText.addEventListener("input", handleTweetTextResize)
+}
+
+function handleTweetTextResize() {
+  this.style.height = ""
+  this.style.height = `${this.scrollHeight}px`
+}
+
+const tweetImageContainer = document.querySelector("[data-hook=tweet-image-container]")
+const tweetImage = document.querySelector("[data-hook=tweet-image]")
+const buttonRemoveMedia = document.querySelector("[data-action=remove-media]")
+
 buttonAddMedia.addEventListener("click", () => inputAddMedia.click())
+inputAddMedia.addEventListener("change", handleTweetAddMedia)
+buttonRemoveMedia.addEventListener("click", handleTweetRemoveMedia)
+
+function handleTweetAddMedia() {
+  const selectedFile = this.files[0]
+
+  if (!selectedFile) return tweetImageContainer.classList.add("is-hidden")
+
+  tweetImage.src = URL.createObjectURL(selectedFile)
+  tweetImageContainer.classList.remove("is-hidden")
+}
+
+function handleTweetRemoveMedia() {
+  tweetImage.src = ""
+  tweetImageContainer.classList.add("is-hidden")
+}
+
+formCreateTweet.addEventListener("submit", handleCreateTweet)
 
 async function handleCreateTweet(event) {
   event.preventDefault()
@@ -22,8 +56,8 @@ async function handleCreateTweet(event) {
   form.reset()
 
   const template = templateTweet.content.cloneNode(true)
-  template.querySelector(`[data-field="id"]`).textContent = id
-  template.querySelector(`[data-field="text"]`).textContent = text
+  template.querySelector("[data-form=tweet]").setAttribute("id", id)
+  template.querySelector("[data-field=text]").textContent = text
 
-  hookTweets.append(template)
+  hookTweets.prepend(template)
 }
