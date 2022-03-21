8ceb5f0aa28bdbc0315f0ffa2c1014cce1135c24
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-21 20:27:04 +0100

    Create user now adds user + user session to the database

--- api/users/users_post.py
+++ api/users/users_post.py
@@ -1,20 +1,20 @@
 from bottle import post, response, request
 
 import jwt
+import mysql.connector
 import re
 import time
 import uuid
 
 from g import (
+    DATABASE_CONFIG,
     JSON_WEB_TOKEN_SECRET,
     REGEX_EMAIL,
     REGEX_PASSWORD,
     REGEX_USERNAME,
-    users,
     USER_NAME_MAX_LENGTH,
     USER_NAME_MIN_LENGTH,
     USER_PASSWORD_MIN_LENGTH,
-    user_sessions,
     USER_USERNAME_MAX_LENGTH,
     USER_USERNAME_MIN_LENGTH,
 )
@@ -24,6 +24,10 @@ from g import (
 def _():
     try:
         ############################################################
+        # Open Database
+        database_connection = mysql.connector.connect(**DATABASE_CONFIG)
+        database_cursor = database_connection.cursor()
+
         # Validate username
         if not request.forms.get("user_username"):
             response.status = 400
@@ -45,10 +49,18 @@ def _():
                 "info": "Username can only have letters from a-z (Uppercase and lowercase), numbers 0-9, underscores and hyphens"
             }
 
-        for user in users:
-            if user["user_username"] == user_username:
-                response.status = 400
-                return {"info": "Username already exists - Forgot password?"}
+        query_username = f"""
+            SELECT *
+            FROM users
+            WHERE user_username = %(user_username)s
+        """
+
+        database_cursor.execute(query_username, {"user_username": user_username})
+        is_username_taken = database_cursor.fetchone()
+
+        if is_username_taken:
+            response.status = 400
+            return {"info": "Username already exists - Forgot password?"}
 
         ############################################################
         # Validate email
@@ -62,10 +74,18 @@ def _():
             response.statis = 400
             return {"info": "Email is not invalid"}
 
-        for user in users:
-            if user["user_email"] == user_email:
-                response.status = 400
-                return {"info": "Email already exists - Forgot your password?"}
+        query_email = f"""
+            SELECT *
+            FROM users
+            WHERE user_email = %(user_email)s
+        """
+
+        database_cursor.execute(query_email, {"user_email": user_email})
+        is_email_registered = database_cursor.fetchone()
+
+        if is_email_registered:
+            response.status = 400
+            return {"info": "Email already exists - Forgot your password?"}
 
         ############################################################
         # Validate password
@@ -102,35 +122,48 @@ def _():
             return {"info": f"Name can only have a maximum of {USER_NAME_MAX_LENGTH} characters"}
 
         ############################################################
+
         # Create user
-        user_id = str(uuid.uuid4())
         user_created_at = int(time.time())
-        user = {
-            "user_id": user_id,
-            "user_created_at": user_created_at,
-            "user_email": user_email,
-            "user_name": user_name,
-            "user_password": user_password,
-            "user_username": user_username,
-        }
+        user = (user_username, user_name, user_email, user_password, user_created_at)
 
         ############################################################
         # Add user
-        users.append(user)
+        add_user = f"""
+            INSERT INTO users (user_id, user_username, user_name, user_email, user_password, user_created_at, user_updated_at, user_bio, user_is_active, user_website, user_location, user_birth_date, user_profile_image, user_cover_image)
+            VALUES (NULL, %s, %s, %s, %s, %s, NULL, '', '1', '', '', NULL, '', '') 
+        """
+
+        database_cursor.execute(add_user, user)
+        database_connection.commit()
 
         ############################################################
+
+        add_user_session = f"""
+            INSERT INTO user_sessions (user_session_id, user_session_iat) 
+            VALUES (%s, %s)
+        """
+
         # Create user session
-        user_session_id = str(uuid.uuid4())
-        user_session = {"session_id": user_session_id, "iat": int(time.time())}
-        user_sessions.append(user_session_id)
+        # user_session = (str(uuid.uuid4()), int(time.time()))
+        user_session_dict = {"user_session_id": str(uuid.uuid4()), "user_session_iat": int(time.time())}
+        user_session_tuple = (user_session_dict["user_session_id"], user_session_dict["user_session_iat"])
+
+        database_cursor.execute(add_user_session, user_session_tuple)
+        database_connection.commit()
 
         ############################################################
         # Success
-        encoded_jwt = jwt.encode(user_session, JSON_WEB_TOKEN_SECRET, algorithm="HS256")
+
+        encoded_jwt = jwt.encode(user_session_dict, JSON_WEB_TOKEN_SECRET, algorithm="HS256")
         response.set_cookie("user_session", encoded_jwt)
         response.status = 201
-        return {"user_id": user_id}
+        return {"user_id": 12312312312}
     except Exception as ex:
         print(ex)
         response.status = 500
         return {"info": "Ups, something went wrong"}
+    finally:
+        if database_connection.is_connected():
+            database_cursor.close()
+            database_connection.close()
