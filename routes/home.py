614996d6328acc88a7f5be5660858d4a59b5c6b5
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-24 07:49:41 +0100

    Home - Show all tweets

--- routes/home.py
+++ routes/home.py
@@ -1,4 +1,4 @@
-from bottle import get, view, redirect, response, request
+from bottle import get, view, redirect, response, request, template
 
 import jwt
 import mysql.connector
@@ -8,6 +8,24 @@ from g import DATABASE_CONFIG, JSON_WEB_TOKEN_SECRET
 
 ############################################################
 @get("/home")
-@view("home")
 def _():
     validate_user_session(None, "/")
+
+    try:
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = connection.cursor(dictionary=True)
+
+        query_get_user_tweets = f"""
+            SELECT *
+            FROM users, tweets
+            WHERE tweets.tweet_fk_user_id = users.user_id
+            ORDER BY tweet_created_at DESC
+        """
+        cursor.execute(query_get_user_tweets)
+        tweets = cursor.fetchall()
+
+        return template("home", dict(tweets=tweets))
+    except Exception as ex:
+        print(ex)
+        response.status = 500
+        return {"info": "Ups, something went wrong"}
