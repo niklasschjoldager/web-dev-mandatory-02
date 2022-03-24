83f6369bc955c83ec0ea5bdbe18c3618c99492e3
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-03-24 07:48:45 +0100

    :sparkles: Delete a tweet

--- api/tweets/tweets_delete.py
+++ api/tweets/tweets_delete.py
@@ -1,11 +1,57 @@
-from bottle import delete
+from bottle import delete, response, request
+import jwt
+import mysql.connector
+import re
+
+from utils.user_session import validate_user_session
+from g import DATABASE_CONFIG, JSON_WEB_TOKEN_SECRET, REGEX_UUID4
 
 ############################################################
 @delete("/tweets/<tweet_id>")
 def _(tweet_id):
-    # Validate
-    # Connect to the db
-    # Delete the tweet
-    # If tweet deleted, send a status of 200
-    # If tweet not found, send a 400 (Correct one: 204)
-    return tweet_id
+    validate_user_session()
+
+    encoded_user_session = request.get_cookie("user_session")
+    user_session = jwt.decode(encoded_user_session, JSON_WEB_TOKEN_SECRET, algorithms=["HS256"])
+
+    try:
+        tweet_id = int(tweet_id)
+        # Validate tweet ID
+        if not tweet_id:
+            response.status = 400
+            return {"info": "Tweet ID is missing"}
+
+        if tweet_id < 1:
+            response.status = 400
+            return {"info": "Tweet ID is not a valid ID"}
+
+        # Connect to the db
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = connection.cursor()
+
+        # Delete the tweet
+        query_delete_tweet = f"""
+            DELETE FROM tweets
+            WHERE tweet_id = %(tweet_id)s AND tweet_fk_user_id = %(user_id)s
+        """
+        # Question - How would you handle this?
+        # Scenario A: User tries to delete tweet that is not his via developer tools -> Error: Unathorized
+        # Scenario B: User tries to delete his own tweet -> Error: Tweet does exist
+        # At the moment they both get "Tweet not found"
+        # Should this be two queries to the DB?
+
+        cursor.execute(query_delete_tweet, {"tweet_id": tweet_id, "user_id": user_session["user_session_fk_user_id"]})
+        connection.commit()
+        is_tweet_deleted = cursor.rowcount
+
+        if not is_tweet_deleted:
+            response.status = 400
+            return {"info": "Tweet not found"}
+
+        # If tweet not found, send a 400 (Correct one: 204)
+        response.status = 200
+        return {"info": "Tweet successfully deleted"}
+    except Exception as ex:
+        print(ex)
+        response.status = 500
+        return {"info": "Ups, something went wrong"}
